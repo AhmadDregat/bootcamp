1313beb7e86cab114d0855733572f46e5a9f6647
ahmad dregat <ahmad.dregat.97@hotmail.com>
2021-04-19 14:47:56 +0300

    start with  java script

---
+++ EX-S/main.js
@@ -0,0 +1,39 @@
+/*console.log("hello , word");
+let myV = "The potential of the future"
+console.log("ahmad is a good way " + myV)
+let password = 123456
+let confirmPassword = 123456
+if (password === confirmPassword) {
+    console.log("success");
+}
+console.log(423 * 12);
+console.log(802 / 2);
+console.log((5 > 2) && false);
+console.log(!("knife" === "sword"));
+console.log((1 < 2) || (-1 > -1) || !false);
+console.log("");
+console.log((31 % 5) == "1");
+console.log(!!true);
+console.log("5th Avenue" != "5th Avenue");
+console.log(52 !== "52");
+console.log((undefined || null));
+
+let a = 3
+a = 4
+let c = 0
+let b = a
+b = 2
+a = b
+b = c
+c = a
+a = b*/
+let a = 3
+a = 4
+let c = 0
+let b = a
+b = 2
+a = b
+b = c
+c = a
+a = b
+console.log(a, b, c);
