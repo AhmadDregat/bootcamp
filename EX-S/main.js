1ae53e183107b8de88710beed29eaee303a267f9
ahmad dregat <ahmad.dregat.97@hotmail.com>
2021-04-19 16:27:41 +0300

    doing some JS

--- EX-S/main.js
+++ EX-S/main.js
@@ -1,13 +1,17 @@
-/*console.log("hello , word");
+/* ex1
+console.log("hello , word");
 let myV = "The potential of the future"
 console.log("ahmad is a good way " + myV)
 let password = 123456
 let confirmPassword = 123456
 if (password === confirmPassword) {
     console.log("success");
-}
+}*/
+/*ex2
 console.log(423 * 12);
 console.log(802 / 2);
+
+/*ex3
 console.log((5 > 2) && false);
 console.log(!("knife" === "sword"));
 console.log((1 < 2) || (-1 > -1) || !false);
@@ -17,7 +21,8 @@ console.log(!!true);
 console.log("5th Avenue" != "5th Avenue");
 console.log(52 !== "52");
 console.log((undefined || null));
-
+*/
+/*ex4
 let a = 3
 a = 4
 let c = 0
@@ -27,6 +32,7 @@ a = b
 b = c
 c = a
 a = b*/
+/*ex5
 let a = 3
 a = 4
 let c = 0
@@ -37,3 +43,56 @@ b = c
 c = a
 a = b
 console.log(a, b, c);
+*/
+/*Exercise 1
+let numChildren = prompt("please enter a num ");
+let isCareful;
+if (!isCareful) {
+    numChildren++
+
+}
+console.log(numChildren);*/
+/*Exercise 2
+let silverwareCount = prompt("please enter a num ");
+if (silverwareCount % 2 == 0) {
+    console.log("even");
+} else { console.log("odd"); }*/
+/*Exercise 3
+let performance = "stellar"
+let salary = 10000
+const goodBonus = 1000
+const stellarBonus = 3000
+if (salary + goodBonus > salary + stellarBonus) {
+    salary += goodBonus
+} else {
+    salary += stellarBonus
+}
+
+console.log(salary);
+*/
+/*Exercise 4
+const isVIP = false
+let cash = 200
+if (isVIP === true || cash > 300) {
+    console.log("you can enter the club");
+} else {
+    console.log("stop you can mot enter the club");
+}
+*/
+/*Exercise 5
+const a = 3
+let b = 2
+let c = 0 || 12
+let d
+
+b = c
+b++
+if (d) {
+    b = a
+}
+
+d = a + (b * c)
+d++
+b += 2
+console.log(a, b, c, d);*/
+/*Exercise 6*/
